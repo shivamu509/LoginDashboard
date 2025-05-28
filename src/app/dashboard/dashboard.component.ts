981c635448e56d0516565d5eab59bf9718405239
shivamSqubix <157590623+shivamSqubix@users.noreply.github.com>
2025-05-28 11:45:13 +0530

    fixed error

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,4 @@
 import { Component, ViewChild } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
 
 interface Employee {
   id: number;
